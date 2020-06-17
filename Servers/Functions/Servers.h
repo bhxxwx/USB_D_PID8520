ead4d777de63874a52894a5286b7189f64590779
ZRH <565309720@qq.com>
2020-06-17 14:02:47 +0800

    初始版本，功能初测一切正常。ZRH第一次提交

--- Servers/Functions/Servers.h
+++ Servers/Functions/Servers.h
@@ -1,32 +1,18 @@
 /*
  * Servers.h
  *
- *  Created on: 2020年4月26日
+ *  Created on: 2020��4��26��
  *      Author: WangXiang
  */
 
 #ifndef SERVERS_H_
 #define SERVERS_H_
-#include "UserConfig.h"
-#include "DATAstruct/DATAstruct.h"
 
+void Get_USB_Message(unsigned char* buffer);
+void Control_light_device(char addh, char addl, char mode, char uart);
+void Control_Scenes_device(char scence_num, char uart);
+void Control_Curtain_device(char addh, char addl, char mode,char uart);
+void Control_Music_device(char addh, char addl, char mode, char uart);
 
-
-
-
-bool iscontants(char *str, char *cmd);
-void send_cmd(char *str);
-bool check_receives(uint8_t cmd_number, char *cmd);
-void BC28_RESET();
-void BC28_Init();
-void pack_to_aliyun();
-
-void writeUTC(char *time, char *data);
-void writeL(char *lan, char *data);
-void clearStr(char *str, uint8_t i);
-
-void anaGPS();
-
-uint8_t get_pack_currentPack(CanRxMsg CANRX);
 
 #endif /* SERVERS_H_ */
