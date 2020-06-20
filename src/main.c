5389dc26f8f5aec833eb1821581cd70b5e2fd706
ZRH <565309720@qq.com>
2020-06-20 15:59:35 +0800

    更新设备号与电路板上的设备号一致。（设备1口：串口2.设备2口：串口3.设备3口：串口1）——ZRH——2020.6.20

--- src/main.c
+++ src/main.c
@@ -30,6 +30,7 @@ int main(void)
 	usart_2_init(9600, 2);
 	usart_3_init(9600, 3);
 	IWDG_INIT(IWDG_Prescaler_64, 1563); //2.5S watch dog
+//	delay_us(500000);
 //	int count = 0, len = 0;
 	while (1)
 	{
@@ -38,104 +39,104 @@ int main(void)
 		IWDG_Feed();
 		if (DeviceControlMessage.status == 1)
 		{
-			if (DeviceControlMessage.DeviceNumber == 0x01)         			//Villa Level 1 or Booth
+			if (DeviceControlMessage.DeviceNumber == 0x01)         			//Villa Level 1 or Booth(设备1口，使用串口2)
 			{
 				switch (DeviceControlMessage.ControlMode)
 				{
 					case 0x00:
-						Control_Scenes_device(DeviceControlMessage.function, 0x01);
+						Control_Scenes_device(DeviceControlMessage.function, 0x02);					//0x01为1号串口
 						break;          //Scene mode, serial port 1
 					case 0x01:
 						Control_light_device(DeviceControlMessage.addh, DeviceControlMessage.addl,
-						        DeviceControlMessage.function, 0x01);
+						        DeviceControlMessage.function, 0x02);
 						break;          //Light mode, serial port 1
 					case 0x03:
 						Control_Curtain_device(DeviceControlMessage.addh, DeviceControlMessage.addl,
-						        DeviceControlMessage.function, 0x01);
+						        DeviceControlMessage.function, 0x02);
 						break;          //Curtain mode, serial port 1
 					case 0x04:
 						Control_Music_device(DeviceControlMessage.addh, DeviceControlMessage.addl,
-						        DeviceControlMessage.function, 0x01);
+						        DeviceControlMessage.function, 0x02);
 						break;          //Music mode, serial port 1
 				}
-			} else if (DeviceControlMessage.DeviceNumber == 0x02) //Villa Level 2 or single-apartment
-			{
-				switch (DeviceControlMessage.ControlMode)
-				{
-					case 0x00:
-						Control_Scenes_device(DeviceControlMessage.function, 0x02);
-						break;          //Scene mode, serial port 2
-					case 0x01:
-						Control_light_device(DeviceControlMessage.addh, DeviceControlMessage.addl,
-						        DeviceControlMessage.function, 0x02);
-						break;          //Light mode, serial port 2
-					case 0x03:
-						Control_Curtain_device(DeviceControlMessage.addh, DeviceControlMessage.addl,
-						        DeviceControlMessage.function, 0x02);
-						break;          //Curtain mode, serial port 2
-					case 0x04:
-						Control_Music_device(DeviceControlMessage.addh, DeviceControlMessage.addl,
-						        DeviceControlMessage.function, 0x02);
-						break;          //Music mode, serial port 2
-				}
-			} else if (DeviceControlMessage.DeviceNumber == 0x03)					//Villa Level 3
+			} else if (DeviceControlMessage.DeviceNumber == 0x02) //Villa Level 2 or single-apartment(设备2口，使用串口3)
 			{
 				switch (DeviceControlMessage.ControlMode)
 				{
 					case 0x00:
 						Control_Scenes_device(DeviceControlMessage.function, 0x03);
-						break;          //Scene mode, serial port 3
+						break;          //Scene mode, serial port 2
 					case 0x01:
 						Control_light_device(DeviceControlMessage.addh, DeviceControlMessage.addl,
 						        DeviceControlMessage.function, 0x03);
-						break;          //Light mode, serial port 3
+						break;          //Light mode, serial port 2
 					case 0x03:
 						Control_Curtain_device(DeviceControlMessage.addh, DeviceControlMessage.addl,
 						        DeviceControlMessage.function, 0x03);
-						break;          //Curtain mode, serial port 3
+						break;          //Curtain mode, serial port 2
 					case 0x04:
 						Control_Music_device(DeviceControlMessage.addh, DeviceControlMessage.addl,
 						        DeviceControlMessage.function, 0x03);
-						break;          //Music mode, serial port 3
+						break;          //Music mode, serial port 2
 				}
-			} else if (DeviceControlMessage.DeviceNumber == 0x04)							//Booth
+			} else if (DeviceControlMessage.DeviceNumber == 0x03)					//Villa Level 3(设备3口，使用串口1)
 			{
 				switch (DeviceControlMessage.ControlMode)
 				{
 					case 0x00:
 						Control_Scenes_device(DeviceControlMessage.function, 0x01);
-						break;          //Scene mode, serial port 1
+						break;          //Scene mode, serial port 3
 					case 0x01:
 						Control_light_device(DeviceControlMessage.addh, DeviceControlMessage.addl,
 						        DeviceControlMessage.function, 0x01);
-						break;          //Light mode, serial port 1
+						break;          //Light mode, serial port 3
 					case 0x03:
 						Control_Curtain_device(DeviceControlMessage.addh, DeviceControlMessage.addl,
 						        DeviceControlMessage.function, 0x01);
-						break;			//Curtain mode, serial port 1
+						break;          //Curtain mode, serial port 3
 					case 0x04:
 						Control_Music_device(DeviceControlMessage.addh, DeviceControlMessage.addl,
 						        DeviceControlMessage.function, 0x01);
-						break;			//Music mode, serial port 1
+						break;          //Music mode, serial port 3
 				}
-			} else if (DeviceControlMessage.DeviceNumber == 0x05)				//single-apartment
+			} else if (DeviceControlMessage.DeviceNumber == 0x04)							//Booth(设备1口，使用串口2)
 			{
 				switch (DeviceControlMessage.ControlMode)
 				{
 					case 0x00:
 						Control_Scenes_device(DeviceControlMessage.function, 0x02);
-						break;          //Scene mode, serial port 2
+						break;          //Scene mode, serial port 1
 					case 0x01:
 						Control_light_device(DeviceControlMessage.addh, DeviceControlMessage.addl,
 						        DeviceControlMessage.function, 0x02);
-						break;          //Light mode, serial port 2
+						break;          //Light mode, serial port 1
 					case 0x03:
 						Control_Curtain_device(DeviceControlMessage.addh, DeviceControlMessage.addl,
 						        DeviceControlMessage.function, 0x02);
-						break;			//Curtain mode, serial port 2
+						break;			//Curtain mode, serial port 1
 					case 0x04:
 						Control_Music_device(DeviceControlMessage.addh, DeviceControlMessage.addl,
 						        DeviceControlMessage.function, 0x02);
+						break;			//Music mode, serial port 1
+				}
+			} else if (DeviceControlMessage.DeviceNumber == 0x05)				//single-apartment(设备3口，使用串口1)
+			{
+				switch (DeviceControlMessage.ControlMode)
+				{
+					case 0x00:
+						Control_Scenes_device(DeviceControlMessage.function, 0x01);
+						break;          //Scene mode, serial port 2
+					case 0x01:
+						Control_light_device(DeviceControlMessage.addh, DeviceControlMessage.addl,
+						        DeviceControlMessage.function, 0x01);
+						break;          //Light mode, serial port 2
+					case 0x03:
+						Control_Curtain_device(DeviceControlMessage.addh, DeviceControlMessage.addl,
+						        DeviceControlMessage.function, 0x01);
+						break;			//Curtain mode, serial port 2
+					case 0x04:
+						Control_Music_device(DeviceControlMessage.addh, DeviceControlMessage.addl,
+						        DeviceControlMessage.function, 0x01);
 						break;			//Music mode, serial port 2
 				}
 			}
